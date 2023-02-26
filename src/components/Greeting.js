acd4f24cd1a0c70fbc4cdbc9e94b9c79fb607467
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2023-02-26 16:19:20 +0100

    my first test

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Greeting() {
+  const [changedText, setChangedText] = useState(false);
+
+  const textChangeHandler = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>It is good to see you here!</p>
+      {!changedText && <p>It is good to see you here!</p>}
+      {changedText && <p>Changed!</p>}
+      <button onClick={textChangeHandler}>Click to make a change</button>
     </div>
   );
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,55 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
-test("renders Hello World as a text", () => {
-  //Arrange
-  render(<Greeting />);
+describe("Greeting Component", () => {
+  test("renders Hello World as a text", () => {
+    //Arrange
+    render(<Greeting />);
 
-  //Act
-  //...Nothing
+    //Act
+    //...Nothing
 
-  //Assert
-  const helloWroldElement = screen.getByText("Hello World!");
-  expect(helloWroldElement).toBeInTheDocument();
+    //Assert
+    const helloWroldElement = screen.getByText("Hello World!");
+    expect(helloWroldElement).toBeInTheDocument();
+  });
+
+  test("Button is not clicked yet", () => {
+    //Arrange
+    render(<Greeting />);
+
+    //Act
+    //...nothing yet
+
+    //Assert
+    const notClickedYet = screen.getByText("It is good to see you here!");
+    expect(notClickedYet).toBeInTheDocument();
+  });
+
+  test("Button is already clicked", () => {
+    //Arrange
+    render(<Greeting />);
+
+    //Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    //Assert
+    const clickedAlready = screen.getByText("Changed!");
+    expect(clickedAlready).toBeInTheDocument();
+  });
+
+  test("Check if the paragraph is not there after clicking", () => {
+    //Arrange
+    render(<Greeting />);
+
+    //Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    //Assert
+    const pTagIsNotThere = screen.queryByText("It is good to see you here!");
+    expect(pTagIsNotThere).toBeNull();
+  });
 });
