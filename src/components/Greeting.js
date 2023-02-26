c7a49d395b52bd068e3c4870a161ef1e59a8d1f5
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2023-02-26 15:45:45 +0100

    my first test

---
+++ src/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Greeting() {
+  return (
+    <div>
+      <h2>Hello World!</h2>
+      <p>It is good to see you here!</p>
+    </div>
+  );
+}
+
+export default Greeting;
