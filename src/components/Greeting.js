5b5833d00f3e418ef800f571e831187302fcafd1
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2023-02-27 16:26:49 +0100

    testing connected components

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Output from "./Output";
 
 function Greeting() {
   const [changedText, setChangedText] = useState(false);
@@ -10,8 +11,8 @@ function Greeting() {
   return (
     <div>
       <h2>Hello World!</h2>
-      {!changedText && <p>It is good to see you here!</p>}
-      {changedText && <p>Changed!</p>}
+      {!changedText && <Output>It is good to see you here!</Output>}
+      {changedText && <Output>Changed!</Output>}
       <button onClick={textChangeHandler}>Click to make a change</button>
     </div>
   );
